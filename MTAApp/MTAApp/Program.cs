25310b8a39405a17cd4e87a720f3c2b6186f4985
Stefan Adrian Catalin <stefan.adrian.m4y@student.ucv.ro>
2023-04-25 16:23:23 +0300

    V1
    v1

--- MTAApp/MTAApp/Program.cs
+++ MTAApp/MTAApp/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using MTAApp.DataAccess.Abstractions;
 using MTAApp.DataAccess.EF;
+using MTAApp.Logic;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
@@ -9,6 +10,8 @@ builder.Services.AddDbContext<DbContext, MTAContext>(options =>
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IPaymentReportRepository, PaymentReportRepository>();
+builder.Services.AddScoped<PaymentReportService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
