03a41792d9ef9464c6642b87c41f084bcdeb70c4
Stefan Adrian Catalin <stefan.adrian.m4y@student.ucv.ro>
2023-05-09 15:56:41 +0300

    Merge "us9" to main
    merge

--- MTAApp/MTAApp.Logic/PaymentReportService.cs
+++ MTAApp/MTAApp.Logic/PaymentReportService.cs
@@ -20,5 +20,25 @@ namespace MTAApp.Logic
         {
             return paymentReportRepository.GetAll();
         }
+
+        public PaymentReport GetPaymentReport(int id)
+        {
+            return paymentReportRepository.Get(id);
+        }
+
+        public PaymentReport AddPaymentReport(PaymentReport paymentReport)
+        {
+            return paymentReportRepository.Add(paymentReport);
+        }
+
+        public PaymentReport UpdatePaymentReport(PaymentReport paymentReport)
+        {
+            return paymentReportRepository.Update(paymentReport);
+        }
+
+        public void DeletePaymentReport(int id)
+        {
+            paymentReportRepository.Remove(id);
+        }
     }
 }
---
+++ MTAApp/MTAApp/Controllers/PaymentReportsController.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MTAApp.DataAccess.Model;
+using MTAApp.Logic;
+using System.Diagnostics.Contracts;
+
+namespace MTAApp.Controllers
+{
+    public class PaymentReportsController : Controller
+    {
+        private readonly PaymentReportService paymentReportService;
+
+        public PaymentReportsController(PaymentReportService prService)
+        {
+            paymentReportService = prService;
+        }
+
+        public ActionResult Index()
+        {
+            var paymentReports = paymentReportService.GetPaymentReports();
+            return View(paymentReports);
+        }
+
+        public ActionResult Details(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var paymentReports = paymentReportService.GetPaymentReport(id);
+            if (paymentReports == null)
+            {
+                return NotFound();
+            }
+
+            return View(paymentReports);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind("Id,EmployeesSalary,ContractsCost,RepairingFund,AppsPayCurrentMonth,AppsPayTotalDebt,OtherPays,Expense,Income,Profit,AssociationId")] PaymentReport paymentReport)
+        {
+            try
+            {
+                paymentReportService.AddPaymentReport(paymentReport);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(paymentReport);
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var paymentReport = paymentReportService.GetPaymentReport(id);
+            if (paymentReport == null)
+            {
+                return NotFound();
+            }
+
+            return View(paymentReport);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, [Bind("Id,EmployeesSalary,ContractsCost,RepairingFund,AppsPayCurrentMonth,AppsPayTotalDebt,OtherPays,Expense,Income,Profit,AssociationId")] PaymentReport paymentReport)
+        {
+            if (id != paymentReport.Id)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                paymentReportService.UpdatePaymentReport(paymentReport);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(paymentReport);
+            }
+            return View(paymentReport);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var paymentReport = paymentReportService.GetPaymentReport(id);
+            if (paymentReport == null)
+            {
+                return NotFound();
+            }
+
+            return View(paymentReport);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                paymentReportService.DeletePaymentReport(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
