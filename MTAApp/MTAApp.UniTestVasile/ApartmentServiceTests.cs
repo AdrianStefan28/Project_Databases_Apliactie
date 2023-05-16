8433b51cace4ed206e51010830b6500765d104dc
VasileGI <92682218+VasileGI@users.noreply.github.com>
2023-05-16 14:54:05 +0300

    3TESTMOKS
    VASILE

--- MTAApp/MTAApp.UniTestVasile/ApartmentServiceTests.cs
+++ MTAApp/MTAApp.UniTestVasile/ApartmentServiceTests.cs
@@ -1,10 +1,41 @@
 using MTAApp.DataAccess.Model;
 using MTAApp.Logic;
+using Moq;
+using MTAApp.DataAccess.Abstractions;
+
 namespace MTAApp.UnitTest.V
 {
     [TestClass]
     public class ApartmentServiceTests
     {
+
+        private Mock<IApartmentRepository> apartmentRepositoryMock = new Mock<IApartmentRepository>();
+
+        [TestInitialize]
+        public void InitializeTests()
+        {
+            Apartment existingProduct = new Apartment
+            {
+                Id = 1,
+                ApNumber = 3,
+                TenantName = "Testx",
+                ColdWater = 52,
+                HotWater = 66,
+                UserId = 1
+            };
+            Apartment existingProduct2 = new Apartment
+            {
+                Id = 2,
+                ApNumber = 6,
+                TenantName = "Test",
+                ColdWater = 21,
+                HotWater = 31,
+                UserId = 1
+            };
+            apartmentRepositoryMock.Setup(pr => pr.Get(1)).Returns(existingProduct);
+            apartmentRepositoryMock.Setup(pr => pr.Get(2)).Returns(existingProduct2);
+        }
+
         [TestMethod]
         public void CalculateWaterPay_Return_WaterPayCost()
         {
@@ -83,6 +114,46 @@ namespace MTAApp.UnitTest.V
             //verify the result to comply with the expected value/s
             Assert.AreEqual("Ion Ionescu", getTenantName);
 
+
+        }
+        [TestMethod]
+        public void GetApartment_Returns_CorrectApartmentWithGivenId()
+        {
+            //arrange
+            ApartmentService service = new ApartmentService(apartmentRepositoryMock.Object);
+            //act
+            var apartment = service.GetApartment(1);
+
+            //assert
+            Assert.IsNotNull(apartment);
+            Assert.AreEqual(1, apartment.Id);
+            Assert.AreEqual("Testx", apartment.TenantName);
+        }
+        [TestMethod]
+        public void GetApartmentByApNumber_Returns_CorrectApartmentWithGivenApNumber()
+        {
+            //arrange
+            ApartmentService service = new ApartmentService(apartmentRepositoryMock.Object);
+            //act
+            var apartment = service.GetApartmentByApNumber(3);
+
+            //assert
+            Assert.IsNotNull(apartment);
+            Assert.AreEqual(1, apartment.Id);
+            Assert.AreEqual(3, apartment.ApNumber);
+        }
+        [TestMethod]
+        public void GetApartmentByTenantName_Returns_CorrectApartmentWithGivenTenantName()
+        {
+            //arrange
+            ApartmentService service = new ApartmentService(apartmentRepositoryMock.Object);
+            //act
+            var apartment = service.GetApartmentByTenantName("Test");
+
+            //assert
+            Assert.IsNotNull(apartment);
+            Assert.AreEqual(2, apartment.Id);
+            Assert.AreEqual("Test", apartment.TenantName);
         }
     }
 }
