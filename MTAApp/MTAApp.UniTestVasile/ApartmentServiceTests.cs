a59d885cd6e66be94e029be5e98664b0364f4e84
Stefan Adrian Catalin <stefan.adrian.m4y@student.ucv.ro>
2023-05-22 10:45:50 +0300

    Revert "Update 6"
    This reverts commit f9ebee7b3f8e76fcc61d8f387780e140739540d0.

---
+++ MTAApp/MTAApp.UniTestVasile/ApartmentServiceTests.cs
@@ -0,0 +1,88 @@
+using MTAApp.DataAccess.Model;
+using MTAApp.Logic;
+namespace MTAApp.UnitTest.V
+{
+    [TestClass]
+    public class ApartmentServiceTests
+    {
+        [TestMethod]
+        public void CalculateWaterPay_Return_WaterPayCost()
+        {
+            //AAA
+            //Arrange
+            //preparing the data for testing
+            Apartment a = new Apartment()
+            {
+                Id = 1,
+                ApNumber = 6,
+                TenantName = "Test",
+                ColdWater = 22,
+                HotWater = 11,
+                UserId = 1
+            }; 
+
+
+            ApartmentService service = new ApartmentService(null);
+
+            //Act
+            //execute the code to be tested
+            var calculateWaterPay = service.CalculateWaterPay(a);
+
+            //Assert
+            //verify the result to comply with the expected value/s
+            Assert.AreEqual(132, calculateWaterPay);
+
+        }
+        [TestMethod]
+        public void CalculateTrashPay_Return_TrashPayCost()
+        {
+            //AAA
+            //Arrange
+            //preparing the data for testing
+            Apartment a = new Apartment()
+            {
+                Id = 1,
+                ApNumber = 3,
+                TenantName = "Test",
+                NoPeople = 3,
+                UserId = 1
+            };
+
+            ApartmentService service = new ApartmentService(null);
+
+            //Act
+            //execute the code to be tested
+            var calculateTrashPay = service.CalculateTrashPay(a);
+
+            //Assert
+            //verify the result to comply with the expected value/s
+            Assert.AreEqual(33, calculateTrashPay);
+
+        }
+        [TestMethod]
+        public void GetTenantName_Return_ApartmentTenantName()
+        {
+            //AAA
+            //Arrange
+            //preparing the data for testing
+            Apartment a = new Apartment()
+            {
+                Id = 1,
+                ApNumber = 3,
+                TenantName = "Ion Ionescu",
+                UserId = 1
+            };
+
+            ApartmentService service = new ApartmentService(null);
+
+            //Act
+            //execute the code to be tested
+            var getTenantName = service.GetApartmentTenantName(a);
+
+            //Assert
+            //verify the result to comply with the expected value/s
+            Assert.AreEqual("Ion Ionescu", getTenantName);
+
+        }
+    }
+}
