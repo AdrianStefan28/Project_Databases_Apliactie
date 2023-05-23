be16a6ee5423276f9376c0a75743f760cc26d0e0
VasileGI <92682218+VasileGI@users.noreply.github.com>
2023-05-23 15:14:44 +0300

    AutomatedTest4Apartment
    Test

---
+++ MTAApp/MTAApp.AutomatedTests/ApartmentsTest.cs
@@ -0,0 +1,44 @@
+﻿using MTAApp.AutomatedTests.PageObjects;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTAApp.AutomatedTests
+{
+    [TestClass]
+     public class ApartmentsTest
+    {
+        private IWebDriver webDriver;
+        [TestInitialize]
+        public void InitTests()
+        {
+            webDriver = new ChromeDriver();
+        }
+
+        [TestMethod]
+        public void ApartmentsAddApartment_Creates_NewApartmentWithGivenTenantName()
+        {
+            Random randomNumber = new Random();
+            string apartmentTenanatName = "MyTestApartment " + randomNumber.Next(100, 10000000);
+            string apartmentApNumber ="" + randomNumber.Next(1, 200);
+            HomePage homePage = new HomePage(webDriver);
+            homePage.GoToPage();
+
+            ApartmentIndexPage indexPage = new ApartmentIndexPage(webDriver);
+            indexPage.GoToPage();
+            AddApartmentPage addApartmentPage = indexPage.GetAddApartmentPage();
+            addApartmentPage.Create(apartmentApNumber, apartmentTenanatName, "1");
+
+            Assert.IsTrue(indexPage.ApartmentExists(apartmentTenanatName));
+        }
+        [TestCleanup]
+        public void Cleanup()
+        {
+            webDriver.Close();
+        }
+    }
+}
